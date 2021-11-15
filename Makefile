a9800ba8416769f35b2ec179bd099aff7eeabb75
cpt-n3m0 <farouq@jouti.co>
2021-11-15 11:37:11 +0200

    updated Makefile and added elsasticity parameter to python wrapper

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-OBJ = heap.o  mvm.o dijkstra.o graph.o
+OBJ = heap.o  mvm.o graph.o
 COMP = gcc -fPIC -g -c -Wall
 
 mvm.so: $(OBJ)
@@ -11,9 +11,6 @@ mvm.o: mvm.c heap.h mvm.h
 	$(COMP) mvm.c
 heap.o:  heap.c heap.h mvm.h
 	$(COMP) heap.c
-dijkstra.o:  dijkstra.c mvm.h dijkstra.h
-	$(COMP) dijkstra.c
-
 graph.o:  graph.c graph.h
 	$(COMP) graph.c
 .PHONY: clean
--- mvm.py
+++ mvm.py
@@ -3,25 +3,25 @@ from cffi import FFI
 
 ffi =  FFI()
 
-# q = [1, 2, 8, 6, 8]
-# t = [1, 2, 9, 3, 3, 5, 9]
+q = [1, 2, 8, 6, 8]
+t = [1, 2, 9, 3, 3, 5, 9]
 
 ffi.cdef("""
     typedef struct match_t {
                   int** matches;
                   float distance;
                   }
-    match_t; match_t mvm(float* query, int lenq, float* target, int lent); 
+    match_t; match_t mvm(float* query, int lenq, float* target, int lent, int elasticity); 
 """)
 mvmlib = ffi.dlopen("./libmvm.so")
 
-def mvm(query, target):
+def mvm(query, target, elasticity=50):
     lq = len(query)
     lt = len(target)
     query = ffi.new(f"float[{lq}]", query)
     target = ffi.new(f"float[{lt}]", target)
     res = ffi.new("match_t *")
-    res = mvmlib.mvm(query, lq, target, lt)
+    res = mvmlib.mvm(query, lq, target, lt, elasticity)
 
     p1 = [res.matches[i][0] for i in range(lq)]
     p2 = [res.matches[i][1] for i in range(lq)]
